049c2a6241fac1629c03c11ac14fce2217ddde4f
cang8 <0.gizemgunes@gmail.com>
2024-01-11 15:07:15 +0300

    03

---
+++ 03_BooleansOperatorsDate/scripts/boolean_values.js
@@ -0,0 +1,15 @@
+// A boolean data type represents one of the two values:true or false. Boolean value is either true or false. The use of these data types will be clear when you start the comparison operator. Any comparisons return a boolean value which is either true or false.
+let isLightOn = true
+let isRaining = false
+let isHungry = false
+let isMarried = true
+let truValue = 4 > 3    // true
+let falseValue = 4 < 3  // false
+
+//Undefined
+let firstName
+console.log(firstName) //not defined, because it is not assigned to a value yet
+
+//Null
+let empty = null
+console.log(empty) // -> null , means no value
